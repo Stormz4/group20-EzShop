75b5bc01d86a4456c02d54555c354c18ada5f4af
Christian Casalini <s281823@studenti.polito.it>
2021-05-03 18:34:31 +0200

    Example for data temporary implementation.

--- src/main/java/it/polito/ezshop/data/EZShop.java
+++ src/main/java/it/polito/ezshop/data/EZShop.java
@@ -3,10 +3,14 @@ package it.polito.ezshop.data;
 import it.polito.ezshop.exceptions.*;
 
 import java.time.LocalDate;
+import java.util.LinkedList;
 import java.util.List;
 
 
 public class EZShop implements EZShopInterface {
+    List<Customer> customers = new LinkedList<>();
+    EZCustomer c1 = new EZCustomer(1, "Ciccio", "00001", 0);
+    // populate customers
 
 
     @Override
@@ -66,7 +70,7 @@ public class EZShop implements EZShopInterface {
 
     @Override
     public List<ProductType> getAllProductTypes() throws UnauthorizedException {
-        List<ProductType> productTypes = null;
+        List<ProductType> productTypes = new LinkedList<>();
         Integer id = 1;
         Integer quantity = 20;
         String location = "C";
