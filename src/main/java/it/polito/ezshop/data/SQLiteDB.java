db1e3076f8f8845a92aed0078de7d2b6dceec01b
Christian Casalini <s281823@studenti.polito.it>
2021-05-04 08:56:56 +0200

    feat: BalanceOperations now handled in DB.

--- src/main/java/it/polito/ezshop/data/SQLiteDB.java
+++ src/main/java/it/polito/ezshop/data/SQLiteDB.java
@@ -1,5 +1,7 @@
 package it.polito.ezshop.data;
 
+import sun.util.resources.LocaleData;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,6 +9,7 @@ import java.sql.DatabaseMetaData;
 import java.sql.Statement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.time.LocalDate;
 import java.util.HashMap;
 
 public class SQLiteDB {
@@ -86,13 +89,13 @@ public class SQLiteDB {
     }
 
     /**
-     ** Create a new Customer table
+     ** Create a new Customers table
      ** EZCustomer(Integer id, String customerName, String customerCard, Integer points)
      */
     public void createCustomerTable() {
         // SQL statement for creating a new Customer table
         // TODO: consider having customerCard UNIQUE
-        String sql = "CREATE TABLE IF NOT EXISTS customers (\n"
+        String sql = "CREATE TABLE IF NOT EXISTS Customers (\n"
                 + " id integer PRIMARY KEY,\n"
                 + " name text NOT NULL,\n"
                 + " card text,\n"
@@ -112,11 +115,11 @@ public class SQLiteDB {
     }
 
     /**
-     ** Select all Customer records
+     ** Select all Customers records
      */
     public HashMap<Integer, EZCustomer> selectAllCustomers(){
         HashMap<Integer, EZCustomer> customers = new HashMap<>();
-        String sql = "SELECT * FROM customers";
+        String sql = "SELECT * FROM Customers";
 
         try {
             Statement stmt  = this.dbConnection.createStatement();
@@ -141,7 +144,7 @@ public class SQLiteDB {
      ** Insert new Customer record
      */
     public Integer insertCustomer(String customerName, String customerCard, Integer points) {
-        String sql = "INSERT INTO customers(name, card, points) VALUES(?,?,?)";
+        String sql = "INSERT INTO Customers(name, card, points) VALUES(?,?,?)";
         Integer customerId = null;
 
         // TODO: Should handle this as an exception?
@@ -170,7 +173,7 @@ public class SQLiteDB {
      ** Delete Customer record with given id
      */
     public void deleteCustomer(Integer id) {
-        String sql = "DELETE FROM customers WHERE id=?";
+        String sql = "DELETE FROM Customers WHERE id=?";
 
         // TODO: Should handle this as an exception?
         if (this.dbConnection == null)
@@ -192,7 +195,7 @@ public class SQLiteDB {
      ** Update Customer record
      */
     public void updateCustomer(Integer customerId, String customerName, String customerCard, Integer points) {
-        String sql = "UPDATE customers\n" +
+        String sql = "UPDATE Customers\n" +
                      "SET name = ?, card = ?, points = ?\n" +
                      "WHERE id = ?;";
 
@@ -211,5 +214,134 @@ public class SQLiteDB {
             System.out.println(e.getMessage());
         }
     }
+
+    /**
+     ** Create a new BalanceOperation table
+     ** EZBalanceOperation (int balanceId, LocalDate date, double money, String type)
+     */
+    public void createBalanceOperationTable() {
+        // SQL statement for creating a new Customer table
+        // TODO: consider having customerCard UNIQUE
+        String sql = "CREATE TABLE IF NOT EXISTS BalanceOperations (\n"
+                + " id integer PRIMARY KEY,\n"
+                + " date text,\n"
+                + " money real,\n"
+                + " type text\n"
+                + ");";
+
+        // TODO: Should handle this as an exception?
+        if (this.dbConnection == null)
+            return;
+
+        try{
+            Statement stmt = this.dbConnection.createStatement();
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     ** Select all BalanceOperations records
+     */
+    public HashMap<Integer, EZBalanceOperation> selectAllBalanceOperations(){
+        HashMap<Integer, EZBalanceOperation> balanceOperations = new HashMap<>();
+        String sql = "SELECT * FROM BalanceOperations";
+
+        try {
+            Statement stmt  = this.dbConnection.createStatement();
+            ResultSet rs    = stmt.executeQuery(sql);
+
+            // loop through the result set
+            while (rs.next()) {
+                Integer id = rs.getInt("id");
+                String strDate = rs.getString("date");
+                LocalDate date = (strDate != null) ? LocalDate.parse(strDate) : null;
+                double money = rs.getDouble("money");
+                String type = rs.getString("type");
+                balanceOperations.put(id, new EZBalanceOperation(id, date, money, type));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return balanceOperations;
+    }
+
+    /**
+     ** Insert new BalanceOperation record
+     */
+    public Integer insertBalanceOperation(LocalDate date, double money, String type) {
+        String sql = "INSERT INTO BalanceOperations(date, money, type) VALUES(?,?,?)";
+        Integer balanceOperationId = null;
+
+        // TODO: Should handle this as an exception?
+        if (this.dbConnection == null)
+            return null;
+
+        String strDate = null;
+        if (date != null)
+            strDate = date.toString();
+
+        try{
+            PreparedStatement pstmt = this.dbConnection.prepareStatement(sql);
+            pstmt.setString(1, strDate);
+            pstmt.setDouble(2, money);
+            pstmt.setString(3, type);
+            pstmt.executeUpdate();
+
+            balanceOperationId = this.lastInsertRowId();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return balanceOperationId;
+    }
+
+    /**
+     ** Delete BalanceOperation record with given id
+     */
+    public void deleteBalanceOperation(Integer id) {
+        String sql = "DELETE FROM BalanceOperations WHERE id=?";
+
+        // TODO: Should handle this as an exception?
+        if (this.dbConnection == null)
+            return;
+
+        if (id == null)
+            return;
+
+        try{
+            PreparedStatement pstmt = this.dbConnection.prepareStatement(sql);
+            pstmt.setInt(1, id);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+//    /**
+//     ** Update BalanceOperation record - TODO: check if we could need to update a BalanceOperation
+//     */
+//    public void updateCustomer(Integer customerId, String customerName, String customerCard, Integer points) {
+//        String sql = "UPDATE BalanceOperations\n" +
+//                "SET name = ?, card = ?, points = ?\n" +
+//                "WHERE id = ?;";
+//
+//        // TODO: Should handle this as an exception?
+//        if (this.dbConnection == null || customerId == null)
+//            return;
+//
+//        try{
+//            PreparedStatement pstmt = this.dbConnection.prepareStatement(sql);
+//            pstmt.setString(1, customerName);
+//            pstmt.setString(2, customerCard);
+//            pstmt.setInt(3, points);
+//            pstmt.setInt(4, customerId);
+//            pstmt.executeUpdate();
+//        } catch (SQLException e) {
+//            System.out.println(e.getMessage());
+//        }
+//    }
 
 }
