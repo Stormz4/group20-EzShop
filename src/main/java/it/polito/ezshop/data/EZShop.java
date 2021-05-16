aba07ffce81c8bb40181ea76c0431dcb7a8bc840
MOVdario <79694881+MOVdario@users.noreply.github.com>
2021-05-16 16:20:56 +0200

    Minor fixes to Luhn algorithm method.

--- src/main/java/it/polito/ezshop/data/EZShop.java
+++ src/main/java/it/polito/ezshop/data/EZShop.java
@@ -1762,6 +1762,9 @@ public class EZShop implements EZShopInterface {
         if(cardNumber == null)
             return false;
 
+        if(!cardNumber.matches("^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\\d{3})\\d{11})$"))
+            return false;
+
         int sum = 0;
         boolean alternate = false;
         for (int i = cardNumber.length() - 1; i >= 0; i--)
@@ -1777,7 +1780,6 @@ public class EZShop implements EZShopInterface {
             alternate = !alternate;
         }
         return (sum % 10 == 0);
-        //return cardNumber.matches("^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\\d{3})\\d{11})$");
     }
 
     public double getCreditInTXTbyCardNumber(String cardNumber)
