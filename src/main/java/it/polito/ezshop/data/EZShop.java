331bc8d013b70c4e2b683705f2112f115268158e
Christian Casalini <s281823@studenti.polito.it>
2021-05-25 17:02:37 +0200

    refactor: better structuring code.

--- src/main/java/it/polito/ezshop/data/EZShop.java
+++ src/main/java/it/polito/ezshop/data/EZShop.java
@@ -244,18 +244,17 @@ public class EZShop implements EZShopInterface {
 
     @Override
     public boolean logout() {
-        if (currUser == null){
+        if (currUser == null)
             return false;
-        }
+
         currUser = null;
         return true;
     }
 
     static public boolean isValidBarCode(String barCode){
 
-        if (barCode == null){
+        if (barCode == null)
             return false;
-        }
 
         if (barCode.matches("[0-9]{12,14}")){
             int sum=0;
@@ -312,6 +311,9 @@ public class EZShop implements EZShopInterface {
     */
 
 
+    //================================================================================================================//
+    //                                                Manage Products                                                 //
+    //================================================================================================================//
     @Override
     public Integer createProductType(String description, String productCode, double pricePerUnit, String note) throws InvalidProductDescriptionException, InvalidProductCodeException, InvalidPricePerUnitException, UnauthorizedException {
         if(this.currUser == null || !this.currUser.hasRequiredRole(URAdministrator, URShopManager))
@@ -447,6 +449,10 @@ public class EZShop implements EZShopInterface {
         return filteredList;
     }
 
+
+    //================================================================================================================//
+    //                                          Manage Inventory and Orders                                           //
+    //================================================================================================================//
     @Override
     public boolean updateQuantity(Integer productId, int toBeAdded) throws InvalidProductIdException, UnauthorizedException {
         if (productId == null || productId<=0){
@@ -475,7 +481,6 @@ public class EZShop implements EZShopInterface {
             return true;
         }
 
-
         return false;
     }
 
@@ -485,6 +490,7 @@ public class EZShop implements EZShopInterface {
 
         return newPos.matches("[0-9]+-[a-zA-z]+-[0-9]+");
     }
+
     @Override
     public boolean updatePosition(Integer productId, String newPos) throws InvalidProductIdException, InvalidLocationException, UnauthorizedException {
         if (productId == null || productId<=0){
@@ -565,27 +571,6 @@ public class EZShop implements EZShopInterface {
         return orderID;
     }
 
-    /**
-     * This method directly orders and pays <quantity> units of product with given <productCode>, each unit will be payed
-     * <pricePerUnit> to the supplier. <pricePerUnit> can differ from the re-selling price of the same product. The
-     * product might have no location assigned in this step.
-     * This method affects the balance of the system.
-     * It can be invoked only after a user with role "Administrator" or "ShopManager" is logged in.
-     *
-     * @param productCode the code of the product to be ordered
-     * @param quantity the quantity of product to be ordered
-     * @param pricePerUnit the price to correspond to the supplier (!= than the resale price of the shop) per unit of
-     *                     product
-     *
-     * @return  the id of the order (> 0)
-     *          -1 if the product does not exists, if the balance is not enough to satisfy the order, if there are some
-     *          problems with the db
-     *
-     * @throws InvalidProductCodeException if the productCode is not a valid bar code, if it is null or if it is empty
-     * @throws InvalidQuantityException if the quantity is less than or equal to 0
-     * @throws InvalidPricePerUnitException if the price per unit of product is less than or equal to 0
-     * @throws UnauthorizedException if there is no logged user or if it has not the rights to perform the operation
-     */
     @Override
     public Integer payOrderFor(String productCode, int quantity, double pricePerUnit) throws InvalidProductCodeException, InvalidQuantityException, InvalidPricePerUnitException, UnauthorizedException {
         if (productCode == null || productCode.isEmpty() || !isValidBarCode(productCode)) {
@@ -623,20 +608,6 @@ public class EZShop implements EZShopInterface {
         return order.getOrderId();
     }
 
-    /**
-     * This method change the status the order with given <orderId> into the "PAYED" state. The order should be either
-     * issued (in this case the status changes) or payed (in this case the method has no effect).
-     * This method affects the balance of the system.
-     * It can be invoked only after a user with role "Administrator" or "ShopManager" is logged in.
-     *
-     * @param orderId the id of the order to be ORDERED
-     *
-     * @return  true if the order has been successfully ordered
-     *          false if the order does not exist or if it was not in an ISSUED/ORDERED state
-     *
-     * @throws InvalidOrderIdException if the order id is less than or equal to 0 or if it is null.
-     * @throws UnauthorizedException if there is no logged user or if it has not the rights to perform the operation
-     */
     @Override
     public boolean payOrder(Integer orderId) throws InvalidOrderIdException, UnauthorizedException {
         if (orderId == null || orderId <=0)
@@ -664,22 +635,6 @@ public class EZShop implements EZShopInterface {
         return true;
     }
 
-    /**
-     * This method records the arrival of an order with given <orderId>. This method changes the quantity of available product.
-     * The product type affected must have a location registered. The order should be either in the PAYED state (in this
-     * case the state will change to the COMPLETED one and the quantity of product type will be updated) or in the
-     * COMPLETED one (in this case this method will have no effect at all).
-     * It can be invoked only after a user with role "Administrator" or "ShopManager" is logged in.
-     *
-     * @param orderId the id of the order that has arrived
-     *
-     * @return  true if the operation was successful
-     *          false if the order does not exist or if it was not in an ORDERED/COMPLETED state
-     *
-     * @throws InvalidOrderIdException if the order id is less than or equal to 0 or if it is null.
-     * @throws InvalidLocationException if the ordered product type has not an assigned location.
-     * @throws UnauthorizedException if there is no logged user or if it has not the rights to perform the operation
-     */
     @Override
     public boolean recordOrderArrival(Integer orderId) throws InvalidOrderIdException, UnauthorizedException, InvalidLocationException {
         if (orderId == null || orderId <=0)
